1c6aab7e675b092b4721bd36aa50a6cf3a1dfcb6
Tim Kopka <tkopka@student.unimelb.edu.au>
2026-04-10 10:30:31 +1000

    Temporary fix to scale inflows

--- src/parser/createGenStorages.jl
+++ src/parser/createGenStorages.jl
@@ -165,6 +165,10 @@ function createGenStorages(storages_input_file, generators_input_file, timeserie
             inflow_data[idx, :] .= round.(Int, gridinjectioncapacity_data[idx, :] * hydro_parameters["default_static_inflow"])
         end
 
+        # Manual fix to scale inflows by unit number (remove later)
+        @warn("Scaling inflows by unit number - remove this temporary fix when the inflow timeseries are updated in PISP.")
+        inflow_data[idx, :] .= inflow_data[idx, :] ./ row.n
+
         # Now determine the initial state of charge
         
         # For the reservoirs, set an initial state of charge via the inflow in the first time step
